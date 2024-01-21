d08950b16dba02dbca2a41a2a556c1d97bc051b0
Percy237 <percylarson17@gmail.com>
2024-01-21 20:02:47 +0100

    Play multiple songs

--- lib/screens/AllSongs.dart
+++ lib/screens/AllSongs.dart
@@ -8,6 +8,8 @@ import "package:just_audio/just_audio.dart";
 import "package:provider/provider.dart";
 import 'package:music_offline_player/provider/song_model_provider.dart';
 
+import "../provider/song_model_provider.dart";
+
 class AllSongs extends StatefulWidget{
   const AllSongs({Key? key}) : super(key: key);
 
@@ -18,6 +20,8 @@ class AllSongs extends StatefulWidget{
 class _AllSongsState extends State<AllSongs> {
   final OnAudioQuery _audioQuery = OnAudioQuery();
   final AudioPlayer _audioPlayer = AudioPlayer();
+
+  List<SongModel> allSongs = [];
 
   playSong(String? uri) {
     try {
@@ -68,30 +72,58 @@ class _AllSongsState extends State<AllSongs> {
               if (item.data!.isEmpty) {
                 return const Center(child: Text("No song found"));
               }
-              return ListView.builder(
-                itemCount: item.data!.length,
-                itemBuilder: (context, index) =>
-                    ListTile(
-                      title: Text(item.data![index].displayNameWOExt),
-                      subtitle: Text("${item.data![index].artist}"),
-                      trailing: const Icon(Icons.more_horiz),
-                      leading: QueryArtworkWidget(
-                        id: item.data![index].id,
-                        type: ArtworkType.AUDIO,
-                        nullArtworkWidget: const Icon(Icons.music_note),
-                      ),
-                      onTap: () {
-                        context.read<SongModelProvider>().setId(item.data![index].id);
+              allSongs.addAll(item.data as Iterable<SongModel>);
+              return Stack(
+                children: [
+                 ListView.builder(
+                    itemCount: item.data!.length,
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+                    itemBuilder: (context, index) =>
+                        ListTile(
+                          title: Text(item.data![index].displayNameWOExt),
+                          subtitle: Text("${item.data![index].artist}"),
+                          trailing: const Icon(Icons.more_horiz),
+                          leading: QueryArtworkWidget(
+                            id: item.data![index].id,
+                            type: ArtworkType.AUDIO,
+                            nullArtworkWidget: const Icon(Icons.music_note),
+                          ),
+                          onTap: () {
+                            context.read<SongModelProvider>().setId(item.data![index].id);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      NowPlaying(songModelList: [item.data![index]],
+                                        audioPlayer: _audioPlayer,),
+                                )
+                            );
+                          },
+                        ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: GestureDetector(
+                      onTap: (){
                         Navigator.push(
                             context,
                             MaterialPageRoute(
                               builder: (context) =>
-                                  NowPlaying(songModel: item.data![index],
+                                  NowPlaying(songModelList: allSongs,
                                     audioPlayer: _audioPlayer,),
                             )
                         );
                       },
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 15, 15),
+                        child: const CircleAvatar(
+                          radius: 30,
+                          child: Icon(Icons.play_arrow),
+                        ),
+                      ),
                     ),
+                  )
+                ],
               );
             }
         )
--- lib/screens/NowPlaying.dart
+++ lib/screens/NowPlaying.dart
@@ -7,10 +7,10 @@ import 'package:music_offline_player/provider/song_model_provider.dart';
 import "package:on_audio_query/on_audio_query.dart";
 import "package:provider/provider.dart";
 
-
 class NowPlaying extends StatefulWidget {
-  const NowPlaying({super.key, required this.songModel, required this.audioPlayer});
-  final SongModel songModel;
+  const NowPlaying(
+      {super.key, required this.songModelList, required this.audioPlayer});
+  final List<SongModel> songModelList;
   final AudioPlayer audioPlayer;
 
   @override
@@ -21,10 +21,21 @@ class _NowPlayingState extends State<NowPlaying> {
   Duration _duration = const Duration();
   Duration _position = const Duration();
 
-
-
   final AudioPlayer _audioPlayer = AudioPlayer();
   bool _isPlaying = false;
+
+  List<AudioSource> songList = [];
+
+  int currentIndex = 0;
+
+  void popBack() {
+    Navigator.pop(context);
+  }
+
+  void seekToSeconds(int seconds) {
+    Duration duration = Duration(seconds: seconds);
+    widget.audioPlayer.seek(duration);
+  }
 
   @override
   void initState() {
@@ -33,23 +44,25 @@ class _NowPlayingState extends State<NowPlaying> {
     playSong();
   }
 
-  void playSong(){
-    try{
-      widget.audioPlayer.setAudioSource(
-          AudioSource.uri(
-              Uri.parse(widget.songModel.uri!),
-            tag: MediaItem(
-              id: '${widget.songModel.id}',
-              album: '${widget.songModel.album}',
-              title: '${widget.songModel.displayNameWOExt}',
-              artUri: Uri.parse('https://example.com/albumart.jpg'),
-            ),
-          )
-      );
+  void playSong() {
+    try {
+      for (var element in widget.songModelList) {
+        songList.add(AudioSource.uri(
+          Uri.parse(element.uri!),
+          tag: MediaItem(
+            id: '${element.id}',
+            album: '${element.album}',
+            title: '${element.displayNameWOExt}',
+            artUri: Uri.parse(element.id.toString()),
+          ),
+        ));
+      }
+      widget.audioPlayer
+          .setAudioSource(ConcatenatingAudioSource(children: songList));
       widget.audioPlayer.play();
       _isPlaying = true;
     } on Exception {
-     log("Cannot parse song");
+      log("Cannot parse song");
     }
     widget.audioPlayer.durationStream.listen((d) {
       setState(() {
@@ -59,6 +72,40 @@ class _NowPlayingState extends State<NowPlaying> {
     widget.audioPlayer.positionStream.listen((p) {
       setState(() {
         _position = p;
+      });
+      listenToEvent();
+      listenToSongIndex();
+    });
+  }
+
+  void listenToEvent() {
+    widget.audioPlayer.playerStateStream.listen((state) {
+      if (state.playing) {
+        setState(() {
+          _isPlaying = true;
+        });
+      } else {
+        setState(() {
+          _isPlaying = false;
+        });
+      }
+      if (state.processingState == ProcessingState.completed) {
+        setState(() {
+          _isPlaying = false;
+        });
+      }
+    });
+  }
+
+  void listenToSongIndex() {
+    widget.audioPlayer.currentIndexStream.listen((event) {
+      setState(() {
+        if (event != null) {
+          currentIndex = event;
+        }
+        context
+            .read<SongModelProvider>()
+            .setId(widget.songModelList[currentIndex].id);
       });
     });
   }
@@ -73,90 +120,104 @@ class _NowPlayingState extends State<NowPlaying> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(onPressed: (){
-                Navigator.pop(context);
-              }, icon: Icon(Icons.arrow_back_ios)),
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back_ios)),
               SizedBox(
                 height: 30.0,
               ),
               Center(
-                child: Column(
-                  children: [
-                  Center(
-                      child: const ArtWorkWidget(),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Text(widget.songModel.displayNameWOExt, overflow: TextOverflow.fade, maxLines: 1, style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0
-                    ), ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Text(widget.songModel.artist.toString(), overflow: TextOverflow.fade, maxLines: 1, style: TextStyle(
-                        fontSize: 20.0
-                    ), ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(_position.toString().split(".")[0]),
-                        Expanded(
-                            child: Slider(
-                              min: const Duration(microseconds: 0).inSeconds.toDouble(),
-                              value: _position.inSeconds.toDouble(),
-                              max: _duration.inSeconds.toDouble(),
-                              onChanged: (value){
-                              setState(() {
-                                changeTopSeconds(value.toInt());
-                                value = value;
-                              });
-                            },)
-                        ),
-                        Text(
-                          _duration.toString().split(".")[0]
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                          onPressed: (){},
-                          icon: const Icon(
-                            Icons.skip_previous,
-                            size: 40.0,
-                           )
-                      ),
-                      IconButton(onPressed: (){
+                  child: Column(children: [
+                Center(
+                  child: ArtWorkWidget(),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text(
+                  widget.songModelList[currentIndex].displayNameWOExt,
+                  overflow: TextOverflow.fade,
+                  maxLines: 1,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  widget.songModelList[currentIndex].artist.toString(),
+                  overflow: TextOverflow.fade,
+                  maxLines: 1,
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(_position.toString().split(".")[0]),
+                    Expanded(
+                        child: Slider(
+                      min: const Duration(microseconds: 0).inSeconds.toDouble(),
+                      value: _position.inSeconds.toDouble(),
+                      max: _duration.inSeconds.toDouble(),
+                      onChanged: (value) {
                         setState(() {
-                          if(_isPlaying){
-                             widget.audioPlayer.pause();
-                          }else {
-                            widget.audioPlayer.play();
-                          }
-                          _isPlaying = !_isPlaying;
+                          changeTopSeconds(value.toInt());
+                          value = value;
                         });
                       },
-                          icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow ,
-                            size: 40.0,)
-                      ),
-                      IconButton(onPressed: (){}, icon: const Icon(Icons.skip_next, size: 40.0)),
-                    ],
-                    )
-                  ]
+                    )),
+                    Text(_duration.toString().split(".")[0]),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          if (widget.audioPlayer.hasPrevious) {
+                            widget.audioPlayer.seekToPrevious();
+                          }
+                        },
+                        icon: const Icon(
+                          Icons.skip_previous,
+                          size: 40.0,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_isPlaying) {
+                              widget.audioPlayer.pause();
+                            } else {
+                              widget.audioPlayer.play();
+                            }
+                            _isPlaying = !_isPlaying;
+                          });
+                        },
+                        icon: Icon(
+                          _isPlaying ? Icons.pause : Icons.play_arrow,
+                          size: 40.0,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          if (widget.audioPlayer.hasNext) {
+                            widget.audioPlayer.seekToNext();
+                          }
+                        },
+                        icon: const Icon(Icons.skip_next, size: 40.0)),
+                  ],
                 )
-              )
+              ]))
             ],
           ),
         ),
       ),
     );
   }
-  void changeTopSeconds(int seconds){
+
+  void changeTopSeconds(int seconds) {
     Duration duration = Duration(seconds: seconds);
     widget.audioPlayer.seek(duration);
   }
@@ -167,7 +228,6 @@ class ArtWorkWidget extends StatelessWidget {
     super.key,
   });
 
-
   @override
   Widget build(BuildContext context) {
     return QueryArtworkWidget(
@@ -176,9 +236,10 @@ class ArtWorkWidget extends StatelessWidget {
       artworkHeight: 200,
       artworkWidth: 200,
       artworkFit: BoxFit.cover,
-      nullArtworkWidget: const Icon(Icons.music_note, size: 200,),
-
+      nullArtworkWidget: const Icon(
+        Icons.music_note,
+        size: 200,
+      ),
     );
   }
 }
-
